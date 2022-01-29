9a17d7128b984d6534b6d765b19692d7432fdedf
steven43533 <steven43533@gmail.com>
2022-01-28 23:54:24 -0500

    added link to github at navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -8,6 +8,7 @@ const Navbar = (props) => {
             <a href="#skills">Skills</a>
             <a href="#projects">Projects</a>
             <a href="https://www.linkedin.com/in/steven-lopez1/" target="_blank">LinkedIn</a>
+            <a href="https://github.com/steven43533" target="_blank">Github</a>
         </div>
 
 
